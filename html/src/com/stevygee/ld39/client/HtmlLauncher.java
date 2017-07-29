1356636bd27a7a39ec0ca9b619c866a161aaf55b
Stefan Grassberger <stefan.grassberger@gmail.com>
2017-07-29 20:49:56 +0200

    libGDX Project

---
+++ html/src/com/stevygee/ld39/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.stevygee.ld39.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.stevygee.ld39.PartyIsland;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new PartyIsland();
+        }
+}
