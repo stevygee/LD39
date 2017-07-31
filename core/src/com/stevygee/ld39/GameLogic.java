5f28f09c83cef354e14d2249b5e134f7304e820e
Stefan Grassberger <stefan.grassberger@gmail.com>
2017-07-31 22:24:41 +0200

    Guests leave after 7 days

--- core/src/com/stevygee/ld39/GameLogic.java
+++ core/src/com/stevygee/ld39/GameLogic.java
@@ -3,6 +3,7 @@ package com.stevygee.ld39;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.utils.IntArray;
 
 public class GameLogic {
 	static boolean isDay = true;
@@ -10,8 +11,9 @@ public class GameLogic {
 
 	// TODO: People per day array (FILO)
 
-	static int guests = 10;
-	static int buildings = 2;
+	static int guests = 0;
+	static IntArray guestsBuffer;
+	static int buildings = 1;
 	static int funds = 10000;
 
 	public static void init() {
@@ -21,8 +23,11 @@ public class GameLogic {
 		PowerManager.init();
 
 		World.addHotel(buildings, 0);
-		World.addBattery(3, 0);
+		World.addBattery(1, 0);
 		World.addSolar(1, 0);
+
+		int[] weekDays = {0,0,0,0,0,0,0};
+		guestsBuffer = new IntArray(weekDays);
 	}
 
 	public static void update(float delta) {
@@ -34,27 +39,39 @@ public class GameLogic {
 			// Tonight's the night...
 			Gdx.app.log("GameLogic", "--- NIGHT ---");
 			Night.init();
-		} else if( !isDay && Night.hasEnded ) {
-			// Night just ended
-			isDay = !isDay;
-			PowerManager.isPowerOn = true;
+		} else if( currentDay == 1 || !isDay && Night.hasEnded ) {
+			if( currentDay != 1 ) {
+				// Night just ended
+				isDay = !isDay;
+				PowerManager.isPowerOn = true;
 
-			Gdx.app.log("Night", "has ended, power failures: " + Night.powerFailures);
+				Gdx.app.log("Night", "has ended, power failures: " + Night.powerFailures);
+			}
 
 			// A new day
-			Gdx.app.log("GameLogic", "--- DAY ---");
+			Gdx.app.log("GameLogic", "--- DAY " + currentDay + " ---");
 
 			float income = guests * 100;
 			gettingMoney(income);
 			Gdx.app.log("GameLogic", "Made " + income + "$, Funds: " + funds + "$");
 
-			int arriving = (int)Math.floor( (guests * 0.1f) + (buildings * 4f) );
-			int leaving = (int)Math.floor( guests * 0.3f * (float)Night.powerFailures );
+			int angryLeaving = (int)Math.floor( guests * 0.3f * (float)Night.powerFailures );
+			int regularLeaving = guestsBuffer.get(currentDay % 7);
+			int leaving = regularLeaving + angryLeaving;
+			int arriving = (int)Math.floor( Math.abs( (guests * 0.1f) + -(angryLeaving * 0.1f) + (buildings * 4f) ) );
+			if( currentDay == 1 ) {
+				arriving = 1;
+			}
 			guests += arriving - leaving;
+			guestsBuffer.set(currentDay % 7, arriving);
+			/*for(int i = 0; i < guestsBuffer.size; i++) {
+				Gdx.app.log("GameLogic", "buffer " + i + ": " + guestsBuffer.get(i));
+			}*/
 			Gdx.app.log("GameLogic", "Good morning! " + arriving + " guests come to the island, " + leaving + " left.");
 			Gdx.app.log("GameLogic", guests + " guests are on the island.");
 
 			Day.reset();
+			currentDay++;
 		}
 
 		// Updates during Day / Night
